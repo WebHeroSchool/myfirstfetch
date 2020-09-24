ac0a42b9be6889add31407807c01b335cc43e963
Serhii Voytov <s.voytov2202@gmail.com>
2020-09-24 15:32:40 +0300

    promise

--- java.js
+++ java.js
@@ -1,44 +1,74 @@
-const body = document.body;
-const urlParam = window.location.search.substring(1);
-const login = (urlParam.split(('='))[1]);
-let url = 'https://api.github.com/users/voytov93';
-if (login) {
-  url = `https://api.github.com/users/${login}`;
-}
+const loading = document.getElementById('loading');
+const clearLoading = new Promise((resolve, reject) => {
+  setTimeout(() => loading ? resolve(loading) : reject('Error'), 2000);
+});
 
-fetch(url)
-  .then(response => {
-    if (response.status !== 404) {
-      return response.json();
-    } else {
-      let err = new Error(response.statusText + ' ' + response.status);
-      err.response = response;
-      throw err;
-    }
+const wrap = document.getElementById('wrap');
+const showContent = new Promise((resolve, reject) => {
+  setTimeout(() => wrap ? resolve(wrap) : reject('Error'), 2000);
+});
+
+const time = new Date();
+const getTime = new Promise((resolve, reject) => {
+  setTimeout(() => time ? resolve(time) : reject('Error'), 2000);
+});
+
+const url = window.location.toString();
+const getUserName = (url) => {
+  let urlSplit = url.split('=');
+  let urlName = urlSplit[1];
+  if (urlName == undefined) {
+    urlName = 'voytov93';
+  }
+  return urlName;
+};
+
+const getName = new Promise((resolve, reject) => {
+  setTimeout(() => getUserName ? resolve(getUserName) : reject('Имя не найдено'), 2000);
+});
+
+Promise.all([clearLoading, showContent, getTime, getName])
+  .then(preloading => loading.style.display = 'none')
+  .then(content => wrap.style.display = 'block')
+  .then(() => fetch(`https://api.github.com/users/${getUserName(url)}`))
+  .then(res => res.json())
+  .then(userInfo => {
+    let name = userInfo.name;
+    let bio = userInfo.bio;
+    let avatar = userInfo.avatar_url;
+    let page = userInfo.html_url;
+
+    let addName = () => {
+      let getName = document.getElementById('userName');
+      getName.innerHTML = name;
+    };
+
+    let addBio = () => {
+      let getBio = document.getElementById('userStatus');
+      getBio.innerHTML = bio;
+    };
+
+    let addAvatar = () => {
+      let getAvatar = document.getElementById('userFoto');
+      getAvatar.setAttribute('src', avatar);
+    };
+
+    let addPage = () => {
+      let getPage = document.getElementById('userUrl');
+      getPage.setAttribute('href', page);
+    };
+
+    let addTime = () => {
+      let nowTime = document.createElement('p');
+      nowTime.innerHTML = time;
+      document.body.appendChild(nowTime);
+    };
+
+    addName();
+    addBio();
+    addAvatar();
+    addPage();
+    addTime();
   })
 
-  .then(json => {
-    let ava = new Image();
-    ava.src = json.avatar_url;
-    body.append(ava);
-
-    let link = document.createElement('a');
-    link.href = json.html_url;
-    link.style.display = "block";
-    if (json.name) {
-      link.innerHTML = json.name;
-    } else {
-      link.innerHTML = 'Информация о пользователе недоступна';
-    }
-    body.appendChild(link);
-
-    let bio = document.createElement('p');
-    if (json.bio) {
-      bio.innerHTML = json.bio;
-    } else {
-      bio.innerHTML = 'Пользователь не заполнил данное поле';
-    }
-    body.append(bio);
-  })
-
-  .catch(error => document.body.innerHTML = `Пользователь не найден.<br> ${error}`);
+  .catch(err => document.body.innerHTML = 'Информация о пользователе не доступна!');
